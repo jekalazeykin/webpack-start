3bd07d97952d90ebbe797ab0d093eecccf343cc6
Dmitry Podgorniy <podgorniy.d@gmail.com>
2017-07-19 22:20:25 +0200

    speedup build by removing uglify during dev builds

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,11 @@ const path = require('path')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const devEnv = 'development'
 const NODE_ENV = process.env.NODE_ENV || devEnv
+const isDev = NODE_ENV === devEnv
 
 module.exports = {
     entry: ['babel-polyfill', './src/index.js'],
-    devtool: NODE_ENV === devEnv ? 'inline-source-map' : 'hidden-source-map',
+    devtool: isDev ? 'inline-source-map' : 'hidden-source-map',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
@@ -17,7 +18,7 @@ module.exports = {
             loader: 'babel-loader'
         }]
     },
-    plugins: [
+    plugins: isDev ? [] : [
         new UglifyJSPlugin({
             sourceMap: true
         })
